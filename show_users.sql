9f575ce3bc8cda3a651935dd347d07bb079327d2
Software Scientist <sheerazali656@gmail.com>
2022-04-14 03:12:51 +0500

    My chocolaty love kinza amjad 💕💕❤️💕💕

---
+++ show_users.sql
@@ -0,0 +1,11 @@
+ show databases;
+ 
+ show databases like "%schema";
+ 
+select schema_name from information_schema.schemata;
+
+select schema_name from information_schema.schemata where schema_name
+	like "k%";
+
+select "kinza amjad is so so chocolaty 💕💕💕";    
+    
