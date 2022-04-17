9b82682eb4eaa1db64e44263a692e41b19f4cdb5
Software Scientist <sheerazali656@gmail.com>
2022-04-17 10:15:29 +0500

    Chocolaty 💕💕💕 Kinza Amjad 💕💕💕

---
+++ create_table.sql
@@ -0,0 +1,16 @@
+
+use kinza_db;
+
+create table kinza_table(
+Id INT NOT NULL AUTO_INCREMENT,
+Name varchar(45) NOT NULL,
+Occupation varchar(35) NOt NULL,
+Age INT NOT NULL,
+PRIMARY KEY(Id)
+);
+
+show tables;
+
+describe kinza_table;
+
+desc kinza_table;
